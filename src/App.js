85acac57ba6f5d43eb3f7fb23eecda86520e1adb
parveen-gitcode <dudekulaparveenatp@gmail.com>
2025-07-24 19:30:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import "./App.css";
+import Accordian from "./components/accordian";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordian component */}
+       <Accordian/> 
+      {/* Random color component */}
+      {/* <RandomColor/> */}
+
+      {/* Star rating component */}
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image slider component */}
+      {/* <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      /> */}
+
+      {/* Load more products component */}
+      {/* <LoadMoreData/> */}
+
+      {/* Tree view component/menu UI component / recursive navigation menu */}
+      {/* <TreeView menus={menus} /> */}
+
+      {/* QR code generator */}
+      {/* <QRCodeGenerator/> */}
+
+      {/* light and dark theme switch */}
+      {/* <LightDarkMode/> */}
+
+      {/* Scroll indicator component */}
+      {/* <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} /> */}
+
+      {/* Custom tabs component */}
+      {/* <TabTest /> */}
+
+      {/* Custom Modal Component */}
+      {/* <ModalTest/> */}
+
+      {/* Github profile finder */}
+      {/* <GithubProfileFinder/> */}
+
+      {/* Search Autocomplete */}
+      {/* <SearchAutocomplete/> */}
+
+      {/* Tic tac toe */}
+      {/* <TicTacToe/> */}
+
+      {/* Feature Flag IMplementation */}
+      {/* <FeatureFlagGlobalState>
+        <FeatureFlags />
+      </FeatureFlagGlobalState> */}
+
+      {/* useFetch - Custom Hook Test*/}
+      {/* <UseFetchHookTest/> */}
+
+      {/* Use Onclick Outside Hook Test */}
+      {/* <UseOnclickOutsideTest/> */}
+
+      {/* Use Window Resize Hook Test */}
+      {/* <UseWindowResizeTest/> */}
+
+      {/* Scroll to Top and Bottom */}
+      {/* <ScrollToTopAndBottom/> */}
+
+      {/* Scroll to a Particular Section */}
+      
+    </div>
+  );
+}
+
+export default App;
